a56f1b1e279dec99e4fe956929a1b1c1610016ac
VuksanFilip <filipvuksan.iphone@gmail.com>
2022-11-30 22:00:44 +0100

    Initialization

---
+++ src/main/java/fab/springapp/App.java
@@ -0,0 +1,9 @@
+package fab.springapp;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
+	}
+}
