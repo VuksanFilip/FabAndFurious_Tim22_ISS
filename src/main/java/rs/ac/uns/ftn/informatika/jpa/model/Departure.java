3395bcc3837557567ea6f13f92aa6630ad1663b1
AndreaSV692020 <andrea.katzenberger01@gmail.com>
2022-12-10 00:01:06 +0100

    Deleted Departure and Destination classes

--- src/main/java/rs/ac/uns/ftn/informatika/jpa/model/Departure.java
+++
@@ -1,8 +0,0 @@
-package rs.ac.uns.ftn.informatika.jpa.model;
-
-public class Departure extends Location{
-
-    public Departure(String address, float longitude, float latitude) {
-        super(address, longitude, latitude);
-    }
-}
--- src/main/java/rs/ac/uns/ftn/informatika/jpa/model/Destination.java
+++
@@ -1,8 +0,0 @@
-package rs.ac.uns.ftn.informatika.jpa.model;
-
-public class Destination extends Location{
-
-    public Destination(String address, float longitude, float latitude) {
-        super(address, longitude, latitude);
-    }
-}
