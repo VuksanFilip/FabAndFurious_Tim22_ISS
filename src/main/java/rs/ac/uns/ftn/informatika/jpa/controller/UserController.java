6ccf468bb984a93ac5e968f1506e41e869f58368
popovbojana <96697478+popovbojana@users.noreply.github.com>
2022-12-09 01:41:53 +0100

    Fixed endpoint for blocking user

--- src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/UserController.java
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/UserController.java
@@ -12,14 +12,19 @@ import rs.ac.uns.ftn.informatika.jpa.model.User;
 public class UserController {
 
     private UserDummy userDummy = new UserDummy();
+    public void addUser(){
+        Integer i = 1;
+        Long id = Long.valueOf(i.longValue());
+        this.userDummy.users.put(id, new User(id, "Pera", "Peric", "picture", "0655555555", "pera.peric@email.com", "address", "pera123"));
+    }
 
     @PutMapping(value = "/{id}/block")
-//    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public ResponseEntity<String> blockUser(@PathVariable Long id) throws Exception {
+        addUser();
         User user = userDummy.users.get(id);
         user.setBlocked(true);
 
-        return new ResponseEntity<String();
+        return new ResponseEntity<String>("User is successfully blocked", HttpStatus.NO_CONTENT);
     }
 
 }
