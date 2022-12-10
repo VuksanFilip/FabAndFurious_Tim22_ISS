906a606b6569f020a28686fbe78598781839bb35
VuksanFilip <filipvuksan.iphone@gmail.com>
2022-12-11 00:47:37 +0100

    #3.1.6 Panic procedure for the ride

--- src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/RideController.java
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/RideController.java
@@ -8,13 +8,19 @@ import org.springframework.web.bind.annotation.*;
 import rs.ac.uns.ftn.informatika.jpa.dto.create.CreateRideDTO;
 import rs.ac.uns.ftn.informatika.jpa.dto.response.RideResponseDTO;
 import rs.ac.uns.ftn.informatika.jpa.dto.response.RideResponseRejectionDTO;
+import rs.ac.uns.ftn.informatika.jpa.dummy.PanicDummy;
 import rs.ac.uns.ftn.informatika.jpa.dummy.RideDummy;
+import rs.ac.uns.ftn.informatika.jpa.model.Panic;
 import rs.ac.uns.ftn.informatika.jpa.model.Ride;
+import rs.ac.uns.ftn.informatika.jpa.model.User;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/ride")
 public class RideController{
     private RideDummy rideDummy = new RideDummy();
+    private PanicDummy panicDummy = new PanicDummy();
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<RideResponseDTO> createRide(@RequestBody CreateRideDTO ride) throws Exception {
@@ -49,5 +55,15 @@ public class RideController{
             return new ResponseEntity<RideResponseRejectionDTO>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<RideResponseRejectionDTO>(ride.parseToResponseRejection(), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{rideId}/panic", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Panic> setPanicReason(@RequestBody String reason, @PathVariable Long rideId) throws Exception {
+        Long panicId = panicDummy.counter.incrementAndGet();
+        Ride ride = rideDummy.rides.get(rideId);
+        Panic panic = new Panic(panicId, new User(), ride, new Date(), reason);
+        panicDummy.panics.put(panicId, panic);
+
+        return new ResponseEntity<Panic>(panic, HttpStatus.OK);
     }
 }
